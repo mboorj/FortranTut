e116d90c9bdbdfaebed202fed3e471d4218ba382
mboorj <mollie.boor@gmail.com>
2023-07-27 17:44:36 -0700

    modified (and broke):   vars.f90

--- vars.f90
+++ vars.f90
@@ -7,4 +7,17 @@ program variables
     character :: initial
     logical :: goingToCrash
 
+    amount = 10
+    pi = 3.14
+    freq = (1.0, -.5)
+    initial = 'A'
+    goingToCrash = .true.
+
+
+    print *, 'our int is: ', amount
+    print *, 'our real is: ', pi
+    print *, 'our complex is: ' freq
+    print *, 'our char is: ', initial
+    print *, 'our bool is: ', goingToCrash
+
 end program variables
